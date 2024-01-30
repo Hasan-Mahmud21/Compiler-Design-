5a9f1802affb71470c8b1ab7f3b9d718b887f22a
Hasan Mahmud <97348227+HasanCoderspace@users.noreply.github.com>
2024-01-30 22:51:10 +0600

    Add files via upload

--- minmaxarr.cpp
+++ minmaxarr.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a[1000], i, n, min, max;
+
+    cout << "Enter size of the array: ";
+    cin >> n;
+
+    cout << "Enter elements in array: ";
+    for (i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    min = max = a[0];
+    for (i = 1; i < n; i++) {
+        if (min > a[i])
+            min = a[i];
+        if (max < a[i])
+            max = a[i];
+    }
+
+    cout << "Minimum of array is: " << min << endl;
+    cout << "Maximum of array is: " << max << endl;
+
+    return 0;
+}
 
